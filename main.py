6efed308045880afc30444eaccc24370063f58ac
Brandon Magaña <brandonmm1208@outlook.com>
2021-12-15 12:10:29 -0800

    Code is documented

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+"""
+    Title: MyPass 
+    Author: BrandonMagana
+    Description: Local Password Administrator using Tkinter module
+"""
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
 import json
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+"""
+    Generates a random password from a set of characters and displays it at 
+    the corresponding entry.
+    Arguments:
+        None
+    Returns:
+        None
+"""
 def generate_password():
     #Clears password field
     password_input.delete(0,END)
@@ -18,16 +32,39 @@ def generate_password():
     
     password_list = password_letters + password_symbols + password_numbers
 
+    #shuffles the random selected characters from each list
     shuffle(password_list)
+
+    #Creates new password
     password = "".join(password_list)
 
+    #Displays password in GUI corresponding field
     password_input.insert(0, password)
-    pyperclip.copy(password)
 
 # ---------------------------- UTIL FUNCTIONS ------------------------------- #
+"""
+    Confirms that all entries have been filled
+    Arguments:
+        * website (str): string that represents the corresponding input for that provided 
+                       in that field
+        * email (str): string that represents the corresponding input for that provided 
+                       in that field
+        * password (str): string that represents the corresponding input for that provided 
+                          in that field
+    Returns:
+        * boolean: True if all entries have been filled, otherwise False
+    
+"""
 def check_entries(website, email, password):
     return len(website) == 0 or len(email) == 0 or len(password)==0
 
+"""
+    Reads data from json file if exists otherwise creates the file
+    Arguments:
+        * file_name (str): string that represents the path of the .json file
+    Returns:
+        * data (dict): if data exists otherwise None
+""" 
 def read_data_from_json(file_name):
     #Reads data from json file if exists or creates it
     try:
@@ -42,30 +79,47 @@ def read_data_from_json(file_name):
 
     return data
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+"""
+    Saves the inputs provided from GUI to .json file
+    Arguments:
+        * None
+    Returns:
+        * None
+"""
 def save_to_json():
     #Getting all inputs from gui
     website = website_input.get().capitalize()
     email= username_input.get().strip().lower()
     password = password_input.get().strip()
     
-    new_data= {
-        website : {
-            "email" : email,
-            "password" : password
-        }
-    }
 
+    #Checking if all inputs have been given properly
     if check_entries(website, email, password):
         messagebox.showinfo(title = "Invalid Entries", message = "Please don't leave any fields empty!")
         return
 
-    #Confirmation message before saving
+    #Confirmation message before saving the info
     is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {email}\n" +
                                                 f"Password: {password}\nIs it okay to save?")
-    #Appending new info to txt file 
+    #Saving info to .json file
     if is_ok:
+        
+        #Dictionary that contains the info to been saved
+        new_data= {
+            website : {
+                "email" : email,
+                "password" : password
+            }
+        }
+
+        #Copying password to clipboard
+        pyperclip.copy(password)
+
+        #Reads .json file or creates a new one if doestn't exists
         data = read_data_from_json("passwords.json")
         
+        #Writes to .json file
         with open(file ="passwords.json", mode ="w") as file:
             if data != None:
                 #Updates the current dictionary with the new keys and values
@@ -74,12 +128,20 @@ def save_to_json():
             else:
                 #Only used when .json file is empty
                 json.dump(new_data, file, indent=4)
-
+        
         #Clear Entry
         website_input.delete(0,END)
         password_input.delete(0,END)
 
 #  ---------------------------- GET WEBSITE INFO ------------------------------- #
+"""
+    Search for website inside .json file, if exits displays a window with the credentials for the website
+    otherwise, shows a window with an error message.
+    Arguments:
+        * None
+    Returns:
+        * None
+"""
 def search_website_info():
     #Getting website name from user entry
     website = website_input.get().capitalize()
@@ -102,17 +164,20 @@ def search_website_info():
         #copying website password to clipboard
         pyperclip.copy(password)
 # ---------------------------- UI SETUP ------------------------------- #
+
+#Creating and configurating window
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20 , pady=20)
 window.grid_columnconfigure(1, weight=1)
 
+#Canvas
 canvas = Canvas(width = 200, height = 200)
 logo_img = PhotoImage(file = "logo.png")
 canvas.create_image(100,100 , image = logo_img)
 canvas.grid(column = 1, row = 0)
 
-#labels
+#Labels
 website_label = Label(text = "Website:", width=25)
 website_label.grid(column= 0, row = 1,)
 username_label = Label(text = "Email/Username:", width=25)
@@ -130,6 +195,7 @@ username_input.insert(END, 'example@test.com')
 password_input = Entry(width=36)
 password_input.grid(column=1, row=3)
 
+#Buttons
 search_btn = Button(text = "Search", width = 25, command = search_website_info)
 search_btn.grid(column=2, row=1)
 gen_pass_btn = Button(text = "Generate Password",width=25, command = generate_password)
